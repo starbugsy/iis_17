3053ee416b47fa36b9eff08ebb26d3ae5123be67
Gabriel Gruber <gabriel.gruber@student.tugraz.at>
2018-01-20 19:05:09 +0100

    first steps

--- acme_client.py
+++ acme_client.py
@@ -3,10 +3,10 @@
 import base64
 import logging
 
-CA = "https://iisca.com"
+DEFAULT_CA = "https://iisca.com"
 LOGGER = logging.getLogger(__name__)
 
-def cert_process(acc_key, csr, acme):
+def cert_process(acc_key, csr, acme_dir, log=LOGGER, CA=DEFAULT_CA):
 
     def b64_help(b):
         return base64.urlsafe_b64encode(b).decode('utf8').replace("=", "")
