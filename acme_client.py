83d019e3f174938ea2eb68dde1a669d891f65ca4
Sandro Letter <sandro@debian-sandro>
2018-01-22 22:39:53 +0100

    test

--- acme_client.py
+++ acme_client.py
@@ -196,8 +196,10 @@ def main(argv):
     parser.add_argument("--acme-dir", required=True, help="path to the .well-known/acme-challenge/ directory")
     parser.add_argument("--quiet", action="store_const", const=logging.ERROR, help="suppress output except for errors")
     parser.add_argument("--ca", default=DEFAULT_CA, help="certificate authority, default is Let's Encrypt")
+    parser.add_argument("--te", required=True, help="help me understand if it works like that!")
 
     args = parser.parse_args(argv)
+    print(args)
     LOGGER.setLevel(args.quiet or LOGGER.level)
     signed_crt = get_crt(args.account_key, args.csr, args.acme_dir, log=LOGGER, CA=args.ca)
     sys.stdout.write(signed_crt)
