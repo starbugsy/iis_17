f6f7219f288c9cb1e09668a1bc6ec1e8a6aa811c
Sandro Letter <sandro@debian-sandro>
2018-01-23 19:13:30 +0100

    script change

--- client_script.sh
+++ client_script.sh
@@ -2,31 +2,37 @@
 
 ###### root.cert.pem eingeben ######
 sudo cp -u /home/iis/certs/root.cert.pem /usr/local/share/ca-certificates
+wait
 
 ###### root.cert.pem umbenenennen ######
 sudo mv /usr/local/share/ca-certificates/root.cert.pem /usr/local/share/ca-certificates/root.cert.crt
+wait
 
 ###### update certificate ######
 sudo update-ca-certificates
+wait
 
 ###### account key ######
 openssl genrsa 4096 > account.key
+wait
 
 ###### domain key ######
 openssl genrsa 4096 > iissite.com.key.pem
+wait
 
 ###### domain csr ######
 openssl req -new -sha256 -key iissite.com.key.pem -subj "/" -reqexts SAN -config <(cat /etc/ssl/openssl.cnf <(printf "[SAN]\nsubjectAltName=DNS:iissite.com,DNS:iissite.com")) > domain.csr
-
-###### boulder activate ######
-#gnome-terminal -e bash bash_for_bolder.sh
+wait
 
 ##### python script ######
-python acme_tiny.py --account-key ./account.key --csr ./domain.csr --acme-dir /var/www/html/.well-known/acme-challenge/ > iissite.com.cert.pem
+python client_for_boulder.py --account-key ./account.key --domain-csr ./domain.csr --acme-dir /var/www/html/.well-known/acme-challenge/ > iissite.com.cert.pem
+wait
 
 ##### final destination ######
 sudo cp -u ./iissite.com.key.pem /home/iis/certs/iissite
+wait
 sudo cp -u ./iissite.com.cert.pem /home/iis/certs/iissite
+wait
 
 ##### reboot apache ######
 sudo systemctl restart apache2
