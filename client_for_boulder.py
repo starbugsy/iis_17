6ff6ea8e729ecbf542b9128b8b4b64529d7b8972
Sandro Letter <sandro@debian-sandro>
2018-01-23 20:35:13 +0100

    client

--- client_for_boulder.py
+++ client_for_boulder.py
@@ -190,7 +190,7 @@ def main(argv):
     parser.add_argument("--domain-csr", required = True, help="path to your certificate signing request")
     parser.add_argument("--acme-dir", required = True, help="path to the .well-known/acme-challenge/ directory")
     #parser.add_argument("--quiet", action="store_const", const=logging.ERROR, help="suppress output except for errors")
-    parser.add_argument("--ca", default=DEFAULT_CA, help="certificate authority, default is Let's Encrypt")
+    parser.add_argument("--ca", default="https://iisca.com", help="certificate authority, default is Let's Encrypt")
 
     arguments = parser.parse_args(argv)
 
