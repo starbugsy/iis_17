e8639b827362848c98324d0f9f53c82fa1ab4288
Sandro Letter <sandro@debian-sandro>
2018-01-23 19:21:12 +0100

    client

--- client_script.sh
+++ client_script.sh
@@ -2,37 +2,37 @@
 
 ###### root.cert.pem eingeben ######
 sudo cp -u /home/iis/certs/root.cert.pem /usr/local/share/ca-certificates
-wait
+sleep 5
 
 ###### root.cert.pem umbenenennen ######
 sudo mv /usr/local/share/ca-certificates/root.cert.pem /usr/local/share/ca-certificates/root.cert.crt
-wait
+sleep 5
 
 ###### update certificate ######
 sudo update-ca-certificates
-wait
+sleep 5
 
 ###### account key ######
 openssl genrsa 4096 > account.key
-wait
+sleep 5
 
 ###### domain key ######
 openssl genrsa 4096 > iissite.com.key.pem
-wait
+sleep 5
 
 ###### domain csr ######
 openssl req -new -sha256 -key iissite.com.key.pem -subj "/" -reqexts SAN -config <(cat /etc/ssl/openssl.cnf <(printf "[SAN]\nsubjectAltName=DNS:iissite.com,DNS:iissite.com")) > domain.csr
-wait
+sleep 5
 
 ##### python script ######
 python client_for_boulder.py --account-key ./account.key --domain-csr ./domain.csr --acme-dir /var/www/html/.well-known/acme-challenge/ > iissite.com.cert.pem
-wait
+sleep 5
 
 ##### final destination ######
 sudo cp -u ./iissite.com.key.pem /home/iis/certs/iissite
-wait
+sleep 5
 sudo cp -u ./iissite.com.cert.pem /home/iis/certs/iissite
-wait
+sleep 5
 
 ##### reboot apache ######
 sudo systemctl restart apache2
